4be3a8ac442d2c1870d3ee8de91349fa6f97ddd2
don1900 <80756375+don1900@users.noreply.github.com>
2021-09-27 10:24:59 +0545

    💝Angel💝

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# AngelUserbot
+# Angel
---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+echo """
+ _   _      _ _______       _   
+| | | |    | | | ___ \     | |  
+| |_| | ___| | | |_/ / ___ | |_ 
+|  _  |/ _ \ | | ___ \/ _ \| __|
+| | | |  __/ | | |_/ / (_) | |_ 
+\_| |_/\___|_|_\____/ \___/ \__|
+                                
+"""
+rm -rf InVade
+git clone https://github.com/don1900/AngelUserbot
+cd InVade
+python3 -m angelbot
